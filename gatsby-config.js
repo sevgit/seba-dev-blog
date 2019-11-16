dd7882b478b7ed0a733d502be74d41b1261515e0
sevgit <sevg1993@gmail.com>
2019-11-16 11:35:36 -0300

    where it all began

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `My Portfolio`,
-    author: `Mohan Talupula`,
+    title: `Seba DEV`,
+    author: `Sebastián Ventura`,
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi recusandae eos vel provident quod similique facilis error vero quisquam omnis?`,
     social: [
       {
         name: `Twitter`,
-        url: `https://twitter.com/gatsbyjs`
+        url: `https://twitter.com/bastianventura`
       },
       {
         name: `GitHub`,
-        url: `https://github.com/gatsbyjs`
+        url: `https://github.com/sevgit`
       }
     ]
   },
